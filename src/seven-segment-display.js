d1d19b95120b32fc492d5b9ee1ddb654118943c5
Kathryn Lenth <kathryn.lenth@gmail.com>
2022-03-28 09:47:20 -0600

    Added SevenSegmentDisplay.reference property

--- src/seven-segment-display.js
+++ src/seven-segment-display.js
@@ -5,15 +5,19 @@ class SevenSegmentDisplay extends React.Component {
     
     render() {
         const s = this.props.state || Array(7).fill(null);
+        const r = this.props.reference || Array(7).fill(null);
+
+        const inc = [...Array(7)].map((_, i) => r[i] !== null && s[i] !== r[i]);
+
         return (
             <div className='seven-segment-display'>
-                <Segment direction='h' x={0} y={0} on={s[0]} />
-                <Segment direction='h' x={0} y={1} on={s[1]} />
-                <Segment direction='h' x={0} y={2} on={s[2]} />
-                <Segment direction='v' x={0} y={0} on={s[3]} />
-                <Segment direction='v' x={0} y={1} on={s[4]} />
-                <Segment direction='v' x={1} y={0} on={s[5]} />
-                <Segment direction='v' x={1} y={1} on={s[6]} />
+                <Segment direction='h' x={0} y={0} on={s[0]} inconsistent={inc[0]} />
+                <Segment direction='h' x={0} y={1} on={s[1]} inconsistent={inc[1]} />
+                <Segment direction='h' x={0} y={2} on={s[2]} inconsistent={inc[2]} />
+                <Segment direction='v' x={0} y={0} on={s[3]} inconsistent={inc[3]} />
+                <Segment direction='v' x={0} y={1} on={s[4]} inconsistent={inc[4]} />
+                <Segment direction='v' x={1} y={0} on={s[5]} inconsistent={inc[5]} />
+                <Segment direction='v' x={1} y={1} on={s[6]} inconsistent={inc[6]} />
             </div>
         );
     }
@@ -25,6 +29,9 @@ function Segment(props) {
         className += ' vertical';
     else
         className += ' horizontal';
+    if (props.inconsistent)
+        className += ' inconsistent';
+
     className += ' x-' + props.x + ' y-' + props.y;
     if (props.on)
         className += ' on';
